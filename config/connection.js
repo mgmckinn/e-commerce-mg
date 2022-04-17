ca489af10c7825ba55f545e8dcc7a89d0d2e5f96
mgmckinn <mgmckinn2325@me.com>
2022-04-16 22:15:14 -0400

    update seed and connection

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,7 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       dialectOptions: {
       decimalNumbers: true,
-      },
-      port: 3001,
+      }
     });
 
 module.exports = sequelize;
